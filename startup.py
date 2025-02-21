dea117be5174708aa9b9a5966edd5575183e3ea1
Chad Gorshing <gorshing@gmail.com>
2025-02-21 09:50:38 -0600

    I use this often when using Python repl
    When I want to use it, I set the PYTHONSTARTUP variable to point to this location

---
+++ startup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from rich import pretty
+from rich import print
+# pretty.install()
+
+print("(In .startup.py)")
+
+import datetime, os, pprint, re, sys, time
+print("(imported datetime, os, pprint, re, sys, time)")
+
+pp = pprint.pprint
